1ce18c4cfc45ece51f006481f9f09d0f89a5c561
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-01-31 13:38:05 -0500

    commit tree d00a6d4360329f3c0bf6b7cceb63a34db3e1ca7b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    const hq = 42;
+   return Math.abs(pickUp - hq);
+}
+function distanceFromHqInFeet(pickUp){
+    const hq = 42;
+    return Math.abs(pickUp - hq) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    const block = 264;
+    return Math.abs(destination - start) * 264;
+}
+function calculatesFarePrice(start, destination){
+    if(Math.abs(destination - start) * 264 < 400){
+        return 0
+    }else if (Math.abs(destination - start)* 264 > 2500){
+        return "cannot travel that far"
+    } else if(Math.abs(destination - start)* 264 >= 2000){
+        return 25 
+    } else if(Math.abs(destination- start) * 264 < 2000) {
+        return (Math.abs((destination - start)* 264)- 400) * .02
+    } 
+}
